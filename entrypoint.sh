b2ed02bf507eed0abc211f5a7e7c632ae2ac41e9
h3xler <101924807+h3xler@users.noreply.github.com>
2025-12-29 14:34:06 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # LiveKit Sunucusuna bağlanıp telefon yönlendirme kuralını oluşturur
-# Parametre hatasını gidermek için --room-name-prefix kullanıyoruz
+# Sadece gerekli olan temel parametreleri bırakıyoruz
 lk sip dispatch create \
   --url "http://livekit:7880" \
   --api-key "$LIVEKIT_API_KEY" \
   --api-secret "$LIVEKIT_API_SECRET" \
   --name "telnyx_rule" \
-  --rule-type individual \
   --room-name-prefix "test_agent" || true
 
 echo "SIP Yönlendirme Kuralı Kontrol Edildi."
